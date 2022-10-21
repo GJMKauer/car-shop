4f3a33485cac7964df785e43151eca0330535214
Gabriel Kauer <gjmkauer@hotmail.com>
2022-10-21 17:52:59 -0300

    FEAT: add reqs 10, 11 and 12 - create test to Car Model, Service and Controller. My coverage is bugged atm, it only shows my CarModel file. Lint is having problems because I've commented every other function from MongoModel and CarService files because I'm not using them yet.

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -9,6 +9,8 @@ describe('Car Model', () => {
 
   beforeEach(() => {
     sinon.stub(mongoose.Model, 'create').resolves(carMockWithId);
+    sinon.stub(mongoose.Model, 'find').resolves([carMockWithId]);
+    sinon.stub(mongoose.Model, 'findOne').resolves(carMockWithId);
   });
 
   afterEach(() => {
@@ -19,6 +21,33 @@ describe('Car Model', () => {
     it('Creates a new car succesfully', async () => {
       const newCar = await carModel.create(carMock);
       expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+  });
+
+  describe('When searching all cars', () => {
+    it('Return all cars succesfully', async () => {
+      const allCars = await carModel.read();
+      expect(allCars).to.be.deep.equal([carMockWithId]);
+    });
+  });
+
+  describe('When searching one car', () => {
+    it('Return a car succesfully when id is found', async () => {
+      const validIdStub = sinon.stub(mongoose, 'isValidObjectId').returns(true);
+      const car = await carModel.readOne('KozukiOden');
+      expect(car).to.be.deep.equal(carMockWithId);
+      validIdStub.restore();
+    });
+
+    it('Return a car succesfully when id is not found', async () => {
+      const invalidIdStub = sinon.stub(mongoose, 'isValidObjectId').returns(false);
+      try {
+        await carModel.readOne('KurozumiOrochi');
+      } catch (err: any) {
+        expect(err.message).to.be.equal('InvalidMongoId');
+      }
+
+      invalidIdStub.restore();
     });
   });
 });
