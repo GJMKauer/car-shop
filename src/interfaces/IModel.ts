ce1bdf01acf604a1a1aae460a24a36a03e2dee6d
Gabriel Kauer <gjmkauer@hotmail.com>
2022-10-21 14:53:51 -0300

    FEAT: add req01 - create IModel generic interface.

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,7 @@
+export interface IModel<T> {
+  create(obj: T): Promise<T>
+  read(): Promise<T[]>
+  readOne(id: string): Promise<T | null>
+  update(id: string, obj: T): Promise<T | null>
+  delete(id: string): Promise<T | null>
+}
